2f10b71a4020b98e9654f50296a6abd484edbc2f
Wouter Wijsman <wwijsman@live.nl>
2026-08-15 00:46:33 +0200

    Fix League title

--- src/menu/League.cpp
+++ src/menu/League.cpp
@@ -6,7 +6,7 @@
 League::League(int number)
 {
   this->number = number;
-  this->title = "Sunday Cup";
+  this->title = std::string("League ") + std::to_string(number);
 
   // Items are added from bottom left to top right
   this->ui_elements.push_back(new UiButton(10.0f, 415.0f, 100.0f, 50.0f, "Exit", Action::OPEN_RACE_MENU));
